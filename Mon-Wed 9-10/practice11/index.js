a0222b2e9d192f5dc34a6e54e61dbf7174e2ff7f
Chantha2121 <chanfcsg334@gmail.com>
2026-04-01 21:17:32 +0700

    update

---
+++ Mon-Wed 9-10/practice11/index.js
@@ -0,0 +1,12 @@
+// console.log('Hello, World!');
+// console.log('Welcome to JavaScript practice!');
+
+var name1 = "Chantha";
+name1 = "Sok";
+const num1 = 2;
+// num1 = 3; // This will cause an error because num1 is a constant
+let name2 = "Sok";
+name2 = "Chantha";
+console.log("Hello, " + name1 + "!");
+console.log("The number is: " + num1);
+console.log("Hello, " + name2 + "!");
